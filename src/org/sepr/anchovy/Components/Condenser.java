1d95fc4d426f9bed2f11db398a1cb208aa865eba
Harrison <hs750@york.ac.uk>
2013-01-06 15:13:40 +0000

    Condenser

---
+++ src/org/sepr/anchovy/Components/Condenser.java
@@ -0,0 +1,72 @@
+package org.sepr.anchovy.Components;
+
+import java.util.Iterator;
+
+import org.sepr.anchovy.InfoPacket;
+import org.sepr.anchovy.Pair;
+import org.sepr.anchovy.Pair.Label;
+
+public class Condenser extends Component {
+	private double temperature;
+	private double pressure;
+	private double waterLevel;
+	public Condenser(String name) {
+		super(name);
+	}
+
+	@Override
+	public InfoPacket getInfo() {
+		InfoPacket info = super.getSuperInfo();
+		info.namedValues.add(new Pair<Double>(Label.temp, temperature));
+		info.namedValues.add(new Pair<Double>(Label.pres, pressure));
+		info.namedValues.add(new Pair<Double>(Label.wLvl, waterLevel));
+		return info;
+	}
+
+	@Override
+	public void calucalte() {
+		temperature = calculateTemp();
+		pressure = calculatePressure();
+		waterLevel = calculateWaterLevel();
+		super.setOuputFlowRate(calculateOutputFlowRate());
+
+	}
+	protected double calculateTemp(){
+		return 0;
+	}
+	protected double calculatePressure(){
+		return 0;
+	}
+	protected double calculateWaterLevel(){
+		return 0;
+	}
+
+	@Override
+	protected double calculateOutputFlowRate() {
+		// TODO Auto-generated method stub
+		// Must distinguish between a pump that pumps steam in with a pump that pumps coolent round (possibly by name of pump having collent or alike in)
+		return 0;
+	}
+
+	@Override
+	public void takeInfo(InfoPacket info) throws Exception {
+		super.takeSuperInfo(info);
+		Iterator<Pair<?>> it = info.namedValues.iterator();
+		Pair<?> pair = null;
+		Label label = null;
+		while(it.hasNext()){
+			pair = it.next();
+			switch(label){
+			case temp:
+				temperature = (Double) pair.second();
+			case pres:
+				pressure = (Double) pair.second();
+			case wLvl:
+				waterLevel = (Double) pair.second();
+			}
+		}
+		
+
+	}
+
+}
